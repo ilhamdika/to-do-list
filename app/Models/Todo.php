91140744f413325448959ecd40061880ddf7d607
ilhamdika <ilham27dika@gmail.com>
2025-03-05 22:29:32 +0700

    feat: model todo

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'assignee',
+        'due_date',
+        'time_tracked',
+        'status',
+        'priority',
+    ];
 }
